b9a15a99d8782f036b13482635868afadc9bfae8
juanluna96 <juandluna17@gmail.com>
2021-03-01 13:39:11 -0500

    Creando el Header

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from '../layouts/Sidebar';
+import Barra from '../layouts/Barra';
 
 const Proyectos = () => {
     return (
         <div className="contenedor-app">
             <Sidebar></Sidebar>
             <div className="seccion-principal">
+                <Barra></Barra>
                 <main>
                     <div className="contenedor-tareas">
 
