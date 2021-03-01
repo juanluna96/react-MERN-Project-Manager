c2218d2e97e1a2d45efe411e2a42c2261d41acd1
juanluna96 <juandluna17@gmail.com>
2021-02-28 20:21:27 -0500

    State y funciones para el Componente

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 
-    const onChange = () => {
-        return
+    // State para iniciar sesión
+    const [usuario, setUsuario] = useState({
+        email: '',
+        password: ''
+    });
+
+    // Extraer del usuario
+    const { email, password } = usuario;
+
+    const onChange = (e) => {
+        setUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Cuando el usuario quiere iniciar sesión
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        // Validar que no haya campos vacíos
+
+        // Pasarlo al action
     }
 
     return (
@@ -11,19 +33,23 @@ const Login = () => {
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar sesión</h1>
 
-                <form action="">
+                <form onSubmit={ onSubmit }>
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" placeholder="Tu correo electrónico" onChange={ onChange } />
+                        <input type="email" name="email" id="email" placeholder="Tu correo electrónico" value={ email } onChange={ onChange } />
                     </div>
                     <div className="campo-form">
                         <label htmlFor="password">Contraseña</label>
-                        <input type="password" name="password" id="password" placeholder="Contraseña" onChange={ onChange } />
+                        <input type="password" name="password" id="password" placeholder="Contraseña" value={ password } onChange={ onChange } />
                     </div>
                     <div className="campo-form">
                         <input type="submit" value="" className="btn btn-primario btn-block" value="Logearse" />
                     </div>
                 </form>
+
+                <Link to={ '/nueva-cuenta' } className="enlace-cuenta">
+                    Registrarme
+                </Link>
             </div>
         </div>
     )
