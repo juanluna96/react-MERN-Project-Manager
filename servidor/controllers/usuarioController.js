197c3accd37891068f82d161ef23c130a8731323
juanluna96 <juandluna17@gmail.com>
2021-03-15 16:59:14 -0500

    Agregando validación a la API

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,8 +1,15 @@
 const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/Usuario')
+const { validationResult } = require('express-validator')
 
 exports.crearUsuario = async(req, res) => {
+
+    // Revisar si hay errores
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() });
+    }
 
     // Extraer email y password
     const { email, password } = req.body;
--- servidor/routes/usuarios.js
+++ servidor/routes/usuarios.js
@@ -1,11 +1,17 @@
 // Rutas para crear usuarios
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController')
 
 // Crea un usuario
 // api/usuarios
 
-router.post('/', usuarioController.crearUsuario);
+router.post('/', [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email valido').isEmail(),
+        check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 })
+    ],
+    usuarioController.crearUsuario);
 
 module.exports = router;
