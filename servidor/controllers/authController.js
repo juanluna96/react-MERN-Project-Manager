87026eef985e7b7fac827eb294cebc66e9070134
juanluna96 <juandluna17@gmail.com>
2021-03-17 12:00:32 -0500

    Revisando si el usuario existe, si el password es correcto o incorrecto

--- servidor/controllers/authController.js
+++ servidor/controllers/authController.js
@@ -9,4 +9,42 @@ exports.autenticarUsuario = async(req, res) => {
     if (!errores.isEmpty()) {
         return res.status(400).json({ errores: errores.array() });
     }
+
+    // Extraer el email y el password
+    const { email, password } = req.body;
+
+    try {
+        // Revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({ email });
+        if (!usuario) {
+            return res.status(400).json({ msg: 'El usuario no existe' });
+        }
+
+        // Revisar el password
+        const passCorrecto = await bcryptjs.compare(password, usuario.password);
+        if (!passCorrecto) {
+            return res.status(400).json({ msg: 'Password incorrecto' });
+        }
+
+        // Si todo es correcto:
+        // Crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        }
+
+        // Firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 //1 Hora
+        }, (error, token) => {
+            if (error) throw error;
+
+            // Mensaje de confirmación
+            res.send({ token });
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
 }
--- servidor/routes/auth.js
+++ servidor/routes/auth.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 router.post('/', [
         check('email', 'Agrega un email valido').isEmail(),
-        check('password', 'El password debe tener minimo de 6 caracteres').isLength({ min: 6 })
+        check('password', 'El password debe tener mínimo de 6 caracteres').isLength({ min: 6 })
     ],
     authController.autenticarUsuario
 );
