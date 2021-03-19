c6df82939a59c8cd3d1274d5b296f5d492f6c7bb
juanluna <juandluna17@gmail.com>
2021-03-18 21:13:19 -0500

    Creando el Modelo

---
+++ servidor/models/Tarea.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tareaSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    estado: {
+        type: boolean,
+        default: false
+    },
+    creado: {
+        type: Date,
+        default: Date.now
+    },
+    proyecto: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Proyecto'
+    }
+});
+
+module.exports = mongoose.model("Tarea", tareaSchema);
