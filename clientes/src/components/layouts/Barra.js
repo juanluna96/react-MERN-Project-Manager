8c1b59c4060d7f1e507197e95cd6602c69296b00
juanluna96 <juandluna17@gmail.com>
2021-03-21 11:25:06 -0500

    Mostrando información del usuario autenticado

--- clientes/src/components/layouts/Barra.js
+++ clientes/src/components/layouts/Barra.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import AuthContext from '../../context/autenticacion/authContext';
 
 const Barra = () => {
+    // Extraer informacion de autenticacion
+    const authContext = useContext(AuthContext);
+    const { usuario, usuarioAutenticado } = authContext;
+
+    useEffect(() => {
+        usuarioAutenticado();
+    }, []);;
+
     return (
         <header className="app-header">
-            <p className="nombre-usuario">Hola <span>Juan Diego</span></p>
-
+            {  usuario ? <p className="nombre-usuario">Hola <span>{ usuario.nombre }</span></p> : null }
             <nav className="nav-principal">
                 <a href="#!">Cerrar sesión</a>
             </nav>
